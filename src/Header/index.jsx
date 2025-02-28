d0b5c6c88dfe43f7ff418b53d7a513caa8a6fe45
jafet <91506409+mmjafet@users.noreply.github.com>
2025-02-28 07:50:35 -0600

    update Header styling and layout; add global CSS reset

--- src/Header/index.jsx
+++ src/Header/index.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className='col-12'>Header
-    <i>bi bi-check</i>
+    <div className='col-12 text-center bg-primary text-white'>
+    <i className='bi bi-check2-square me-1'></i>To do list
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.min.js'
 import 'bootstrap-icons/font/bootstrap-icons.css'
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>

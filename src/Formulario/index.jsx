8fe27551b11b8c87804fd155b99c3a2d26762cb0
jafet <91506409+mmjafet@users.noreply.github.com>
2025-03-19 15:27:36 -0600

    refactor Formulario and Tarea components for improved layout and functionality; add form elements and buttons

--- src/Formulario/index.jsx
+++ src/Formulario/index.jsx
@@ -1,13 +1,28 @@
 import React from 'react'
 
 const Formulario = () => {
-  return (
-    <div>
-        <label >Tarea</label>
-        <input type="text" placeholder='Hacer Tarea' />
-        <label htmlFor="Descripcion"></label>
-        
+  return (<>
+    <form className='flex flex-column col-9'>
+        <h1 className='text-center'>To - Do List</h1>
+        <div className='input-group mb-3 col-12'>
+
+            <label className='input-group-text'>Tarea</label>
+            <input type="text" placeholder='Hacer Tarea' required className='form-control'/>
+        </div>
+        <div className='input-group'>
+            
+            <label htmlFor="Descripcion" className='input-group-text'></label>
+            <input type="text" placeholder='Descripcion' className='form-control'/>
+            <button className='btn btn-outline-success btn-lg'>Agregar</button>
+
+        </div>
+    </form>
+    <div className='m-3 btn-group col-9'> 
+        <div className="btn btn-outline-primary btn-lg">Todos</div>
+        <div className="btn btn-outline-secondary btn-lg">Pendientes</div>
+        <div className="btn btn-outline-warning btn-lg">Completados</div>
     </div>
+  </>
   )
 }
 
--- src/Tareas/index.jsx
+++ src/Tareas/index.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Tarea = () => {
   return (
-    <div>Tarea</div>
+    <section 
+    className='flex flex-column justify-content-center align-items-center col-9'>
+        <div className="card col-12" >
+            <div className='col-8'>
+                <h3 className='text-primary'>Deploy</h3>
+                <p>Desplegar app en Netlify</p>
+            </div>
+
+        </div>
+
+</section>
   )
 }
 
